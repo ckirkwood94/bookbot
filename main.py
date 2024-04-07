1a2e65f297a1a7494a9434b13abecff19edbbab1
ckirkwood94 <ckirkwood94@gmail.com>
2024-04-07 01:33:22 -0400

    Added function to count letters and sort them and compile info into a report

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     frankenstein_path = "books/frankenstein.txt"
     frankenstein_text = get_book_text(frankenstein_path)
-    # print(get_book_text(frankenstein_path))
-    print(count_words(frankenstein_text))
+    frankenstein_word_count = count_words(frankenstein_text)
+    frankenstein_letter_count = count_letters(frankenstein_text)
+    sorted_frankenstein_letter_count = sort_letter_count(frankenstein_letter_count)
+
+    book_report(frankenstein_path, frankenstein_word_count, sorted_frankenstein_letter_count)
 
 
 def get_book_text(book_path):
@@ -12,5 +15,41 @@ def get_book_text(book_path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    letter_count = {}
+    lowercase_text = text.lower()
+    for char in lowercase_text:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+    return letter_count
+
+def sort_letter_count(dict):
+    def sort_on(dict):
+        return dict["count"]
+    
+    def create_dict_list(dict):
+        dict_list = []
+        for key in dict:
+            if key.isalpha():
+                dict_list.append({"char": key, "count": dict[key]})
+        return dict_list
+    
+    list_of_dict = create_dict_list(dict)
+    list_of_dict.sort(reverse=True, key=sort_on)
+
+    return list_of_dict
+
+def book_report(path, word_count, letter_count):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document \n")
+    for dict in letter_count:
+        char = dict["char"]
+        count = dict["count"]
+        print(f"The '{char}' character was found {count} times")
+    print("\n--- End Report ---")
+    pass
 
 main()
